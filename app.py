2929a05e420ac79a9a0d1c58d36d8ea5157fab59
d12eek <deeksha6goyal@gmail.com>
2026-06-18 19:27:13 +0530

    Fix: final app.py and requirements.txt for Render

--- app.py
+++ app.py
@@ -14,13 +14,12 @@ app = Flask(__name__)
 load_dotenv()
 
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
-GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
+GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
 
 embeddings = download_hugging_face_embeddings()
 
 index_name = "medicalbot"
 
-# Load existing Pinecone index
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
     embedding=embeddings
@@ -31,13 +30,11 @@ retriever = docsearch.as_retriever(
     search_kwargs={"k": 3}
 )
 
-GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
 llm = ChatGroq(
     model="llama-3.3-70b-versatile",
     api_key=GROQ_API_KEY,
     temperature=0.4,
     max_tokens=500
-
 )
 
 prompt = ChatPromptTemplate.from_messages(
@@ -57,20 +54,17 @@ rag_chain = (
     | StrOutputParser()
 )
 
-
 @app.route("/")
 def index():
     return render_template('chat.html')
 
-
 @app.route("/get", methods=["GET", "POST"])
 def chat():
     msg = request.form["msg"]
-    input = msg
-    print(input)
+    print(msg)
     response = rag_chain.invoke(msg)
     return str(response)
 
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
